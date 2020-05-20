9fe91a839848cb1aa44997505751908d511d1a11
asamas <r.semba0124@gmail.com>
2020-05-20 22:37:36 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-'use strict';
-function fib(n){
-    if (n===0) {
-        return 1
-    }else if (n===1){
-        return 1
-    }else{
-        return fib(n-1) + fib(n-2)
-    }
+"use strict";
+const memo = new Map();
+memo.set(0, 1);
+memo.set(1, 1);
+function fib(n) {
+  if (memo.has(n)) {
+    return memo.get(n);
+  } else {
+    const value = fib(n - 1) + fib(n - 2);
+    memo.set(n, value);
+    return value;
+  }
 }
 
 const length = 40;
-for (let i = 0; i <= length ; i++){
-    console.log(fib(i))
+for (let i = 0; i <= length; i++) {
+  console.log(fib(i));
 }
